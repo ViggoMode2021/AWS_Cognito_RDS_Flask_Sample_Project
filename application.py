bd54e8c4229bd985ff01ca4f21d69749465f6627
Ryan <ryansviglione@gmail.com>
2022-08-22 14:05:45 -0400

    Commit

--- application.py
+++ application.py
@@ -1,71 +1,72 @@
-from flask import Flask, render_template, request, flash, session, redirect, url_for
-import psycopg2
+from flask import Flask, render_template, request, flash, session, redirect, url_for # Backend for application
+import psycopg2 # Database adapter for Python to Postgres
 import psycopg2.extras
 import os
-import boto3
-from dotenv import load_dotenv, find_dotenv
-import datetime
+import boto3 # AWS Python SDK
+from dotenv import load_dotenv, find_dotenv # Access .env files
+import datetime # To record dates and times
 
 application = Flask(__name__)
 
 load_dotenv(find_dotenv())
 
-dotenv_path = os.path.join(os.path.dirname(__file__), ".env_aws_flask_services")
+dotenv_path = os.path.join(os.path.dirname(__file__), ".env_aws_flask_services") # Name of .env file
 load_dotenv(dotenv_path)
 
-application.secret_key = os.getenv("SECRET_KEY")
+application.secret_key = os.getenv("SECRET_KEY") # Flask secret key
 
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
+DB_HOST = os.getenv("DB_HOST") # RDS Database endpoint
+DB_NAME = os.getenv("DB_NAME") # RDS Database name
+DB_USER = os.getenv("DB_USER") # RDS Database username
+DB_PASS = os.getenv("DB_PASS") # RDS Database password
 
-aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'),
-aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY')
+aws_access_key_id = os.getenv('AWS_ACCESS_KEY_ID'), # Access key for IAM user
+aws_secret_access_key = os.getenv('AWS_SECRET_ACCESS_KEY') # Secret access key for IAM user
 
-conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
-cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
+conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST) # Establish connection to Postgres from Flask
+cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) # Used to traverse records from the query
 
 @application.route('/')
 def sign_up():
-    return render_template("sign_up.html")
+    return render_template("sign_up.html") # Landing page
 
-@application.route('/sign_up_submit', methods=['POST'])
+@application.route('/sign_up_submit', methods=['POST']) # Function to sign up
 def sign_up_submit():
     try:
-        email_sign_up = request.form.get("email_sign_up")
-        password_sign_up = request.form.get("password_sign_up")
+        email_sign_up = request.form.get("email_sign_up") # Input box in 'sign_up.html
+        password_sign_up = request.form.get("password_sign_up") # Password box in 'sign_up.html
 
-        client = boto3.client("cognito-idp", region_name="us-east-1")
+        client = boto3.client("cognito-idp", region_name="us-east-1") # Client for Boto3 Cognito
         client.sign_up(
-            ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
+            ClientId=os.getenv("COGNITO_USER_CLIENT_ID"), # Client ID for Cognito User Pool
             Username=email_sign_up,
             Password=password_sign_up,
-            UserAttributes=[{"Name": "email", "Value": email_sign_up}],
+            UserAttributes=[{"Name": "email", "Value": email_sign_up}], # Attributes sent to User Pool
         )
 
-        session['loggedin'] = True
+        session['loggedin'] = True # Establish a session to keep the user logged in and capable of accessing authentication page
 
-        session['username'] = email_sign_up
+        session['username'] = email_sign_up # Establish a username variable to query tables and render a HTML variable
 
-        return redirect(url_for('authenticate_page'))
+        return redirect(url_for('authenticate_page')) # If initial sign up is successful, send user to the authentication page
     except:
-        return redirect(url_for('authenticate_page'))
+        flash('Username or password incorrect, or user is not in the system.')
+        return redirect(url_for('sign_up')) # Return user to sign up page if try block fails. This could be due to user already being in system.
 
-@application.route('/authenticate_page', methods=['GET'])
+@application.route('/authenticate_page', methods=['GET']) # Route user to authentication page
 def authenticate_page():
     if 'loggedin' in session:
         return render_template('authenticate.html')
 
     return redirect(url_for('sign_up'))
 
-@application.route('/authenticate', methods=['POST'])
+@application.route('/authenticate', methods=['POST']) # Function for user to input authentication code
 def authenticate():
     if 'loggedin' in session:
-        authentication_code = request.form.get('authentication_code')
+        authentication_code = request.form.get('authentication_code') # Input box on authenticate.html
         client = boto3.client("cognito-idp", region_name="us-east-1", use_ssl=True)
 
-        client.confirm_sign_up(
+        client.confirm_sign_up( # Confirms user in Cognito user pool if the authentication code is valid
             ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
             Username=session['username'],
             ConfirmationCode=authentication_code,
@@ -78,31 +79,31 @@ def authenticate():
 
         format_code = '%m-%d-%Y'
 
-        date_object = date.strftime(format_code)
+        date_object = date.strftime(format_code) # Establish date to input into db
 
         conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
         cursor.execute(
             "INSERT INTO username (username, overall_score, account_creation_date) VALUES (%s, %s, %s);",
-            (session['username'], 0, date_object))
+            (session['username'], 0, date_object)) # Insert user, overall score, and registration date into db
 
-        conn.commit()
+        conn.commit() # Commit insert
 
-        cursor.close()
-        conn.close()
+        cursor.close() # Close cursor
+        conn.close() # Close connection
 
-        session.pop('username')
+        session.pop('username') # Remove session
 
-        return redirect(url_for('sign_up'))
+        return redirect(url_for('sign_up')) # Return to sign up page
 
     return redirect(url_for('sign_up'))
 
-@application.route('/login_page', methods=['POST', 'GET'])
+@application.route('/login_page', methods=['POST', 'GET']) # Login page
 def login_page():
     return render_template('sign_up.html')
 
-@application.route('/login', methods=['POST', 'GET'])
+@application.route('/login', methods=['POST', 'GET']) # Login function
 def login():
     try:
         username = request.form.get('username')
@@ -110,7 +111,7 @@ def login():
 
         client = boto3.client("cognito-idp", region_name="us-east-1")
 
-        client.initiate_auth(
+        client.initiate_auth( # Check if user in User Pool
             ClientId=os.getenv("COGNITO_USER_CLIENT_ID"),
             AuthFlow="USER_PASSWORD_AUTH",
             AuthParameters={"USERNAME": username, "PASSWORD": password},
@@ -120,31 +121,31 @@ def login():
 
         session['username'] = username
 
-        return redirect(url_for('level_selector'))
+        return redirect(url_for('home')) # Bring user to home page
 
     except:
         flash('Username or password incorrect, or user is not in the system.')
         return redirect(url_for('sign_up'))
 
-@application.route('/home', methods=['GET'])
+@application.route('/home', methods=['GET']) # Send user to home page
 def home():
     if 'loggedin' in session:
 
         conn = psycopg2.connect(dbname=DB_NAME, user=DB_USER, password=DB_PASS, host=DB_HOST)
         cursor = conn.cursor(cursor_factory=psycopg2.extras.DictCursor)
 
-        cursor.execute('SELECT overall_score FROM users WHERE username = %s;', [session['username']])
+        cursor.execute('SELECT overall_score FROM users WHERE username = %s;', [session['username']]) # Query for overall score
 
-        overall_score = cursor.fetchone()
+        overall_score = cursor.fetchone() # Query and fetch data
 
-        session['overall_score'] = overall_score[0]
+        session['overall_score'] = overall_score[0] # Remove brackets from fetched data
 
         cursor.close()
         conn.close()
 
-        return render_template('level_selector.html', username=session['username'], overall_score=session['overall_score'])
+        return render_template('home.html', username=session['username'], overall_score=session['overall_score'])
 
     return redirect(url_for('login'))
 
 if __name__ == '__main__':
-    application.run(debug=True)
+    application.run(debug=True) # Run Flask with the debugger
